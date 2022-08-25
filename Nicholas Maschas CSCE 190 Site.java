f4ab0e0611b869c733eec51bdf57df4b622f4bd4
Skyl96 <112007497+Skyl96@users.noreply.github.com>
2022-08-24 20:56:27 -0400

    Update Nicholas Maschas CSCE 190 Site.java

--- Nicholas Maschas CSCE 190 Site.java
+++ Nicholas Maschas CSCE 190 Site.java
@@ -1,1 +1,9 @@
 Nicholas Maschas CSCE 190 Site
+ cd /path/to/local/folder
+ git init .
+ git config user.name <YourGitHubAccount>
+ git config user.email <YourGitHubAccountEmail>
+ git add .
+ git commit -m "Add folder content"
+ git remote add origin https://github.com/<YourGitHubAccount>/<YourRepo>.git
+ git push -u origin master
